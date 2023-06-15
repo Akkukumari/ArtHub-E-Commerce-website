3ed906e898758b047de576cc52a54ec1c72a4fb1
Bipin Singh Bhat <bipinsinghbhat1698@gmail.com>
2023-06-15 20:01:17 +0530

    logsign

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 // import { store } from "./redux/store";
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from "react-router-dom";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
